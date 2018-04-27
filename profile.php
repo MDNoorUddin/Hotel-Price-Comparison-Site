c50615cfedb99b603de00d2d864cf8c29a4c7548
MD. Noor Uddin <31165772+MDNoorUddin@users.noreply.github.com>
2018-04-27 09:53:12 +0600

    Profile of Developer

---
+++ profile.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  max-width: 300px;
+  margin: auto;
+  text-align: center;
+  font-family: arial;
+}
+
+.title {
+  color: grey;
+  font-size: 18px;
+}
+
+button {
+  border: none;
+  outline: 0;
+  display: inline-block;
+  padding: 8px;
+  color: white;
+  background-color: #000;
+  text-align: center;
+  cursor: pointer;
+  width: 100%;
+  font-size: 18px;
+}
+
+a {
+  text-decoration: none;
+  font-size: 22px;
+  color: black;
+}
+
+button:hover, a:hover {
+  opacity: 0.7;
+}
+</style>
+</head>
+<body>
+
+<h2 style="text-align:center">Profile Card</h2>
+
+<div class="card" align="center">
+  <img src="IMG_20171013_163712 - Copy.jpg" alt="John" style="width:100%">
+  <h1>MD. Noor Uddin</h1>
+  <p class="title">CEO & Founder, Example</p>
+  <p>Pabna University of Science & technology</p>
+  <div style="margin: 24px 0;">
+    <a href="#"><i class="fa fa-phone"></i></a> 
+    <a href="https://github.com/MDNoorUddin"><i class="fa fa-github"></i></a>  
+    <a href="#"><i class="fa fa-uva"></i></a>  
+    <a href="https://web.facebook.com/profile.php?id=100005324868507"><i class="fa fa-facebook"></i></a> 
+ </div>
+ <p><button onclick="www.gmail.com">Contact</button></p>
+</div>
+
+</body>
+</html>
